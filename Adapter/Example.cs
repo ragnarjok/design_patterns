d1f2e0ee89cb92ceb5b9eeb44b24f313d45230bb
Iryna Shakhova <irina.shakhovaya@gmail.com>
2014-03-06 14:16:16 +0100

    Bridge & Adapter patterns

---
+++ Adapter/Example.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Adaptee
+    {
+        public void ExecuteClassSpecificAction()
+        {
+            Console.WriteLine("class-specific logic");
+        }
+    }
+
+    interface ITarget
+    {
+        void ExecuteInterfaceSpecificAction ();
+    }
+
+    class Adapter : Adaptee, ITarget
+    {
+        public void ExecuteInterfaceSpecificAction ()
+        {
+           ExecuteClassSpecificAction ();
+        }
+    }
+
+    class Client
+    {
+        static void Main ()
+        {
+            var first = new Adaptee ();
+            Console.Write ("Before the new standard: ");
+            first.ExecuteClassSpecificAction ();
+
+            ITarget second = new Adapter ();
+            Console.WriteLine ("\nMoving to the new standard");
+            second.ExecuteInterfaceSpecificAction () ;
+        }
+    }	
+
+
+}
---
+++ Bridge/Example.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Bridge
+{
+    public class Example
+    {
+    }
+
+    public interface IBridgeComponents
+    {
+        void Send (string dataType);
+    }
+
+    public abstract class Client
+    {
+        public IBridgeComponents Components;
+        public abstract void Send ();
+    }
+
+    public class EmailClientImpl : Client
+    {
+        public override void Send ()
+        {
+            Components.Send ("Email");
+        }
+    }
+
+    public class SmsClientImpl : Client
+    {
+        public override void Send ()
+        {
+            Components.Send ("Sms");
+        }
+    }
+
+    public class WebService : IBridgeComponents
+    {
+        public void Send (string dataType)
+        {
+            Console.WriteLine("Sending "+dataType+" using WebService");
+        }
+    }
+
+    public class ThirdPatry : IBridgeComponents
+    {
+        public void Send (string dataType)
+        {
+            Console.WriteLine("Sending "+dataType+" using 3rd-party library");
+        }
+    }
+}
